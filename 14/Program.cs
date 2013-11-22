05f4f10f26bc35dc00e1b4172014f7990b832f17
sportsman <wazzaappp.1994@yahoo.com>
2013-11-23 00:51:25 +0200

    .init

---
+++ 14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("you should enter a number "+
+                "that has an exact square root");
+            Console.WriteLine("the number of the symbols is");
+            int numberOfSymbols = int.Parse(Console.ReadLine());
+            int rows = (int)Math.Sqrt(numberOfSymbols);
+            char symbol = '\u00A9';
+            int count = 1;
+            for (int currentRow = 1; currentRow <= rows; currentRow++)
+            {
+                for (int space = 0; space < rows-currentRow; space++)
+                {
+                    Console.Write(" ");
+                }
+                if (count==1)
+                {
+                    Console.Write(symbol);
+                    count++;
+                    Console.WriteLine();
+                    continue;                   
+                }
+                for (int printingSymbol = 1; printingSymbol <= 1+2*(currentRow-1); printingSymbol++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
